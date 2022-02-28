47e43b59db32b5083e88b856df7194c011c1a8fd
Udit Mittal <23015406+udit-001@users.noreply.github.com>
2022-02-28 23:14:28 +0530

    Add django admin for models

--- youtube_search/videos/admin.py
+++ youtube_search/videos/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from videos.models import VideoResult, SearchQuery
 
-# Register your models here.
+
+@admin.register(VideoResult)
+class VideoResultAdmin(admin.ModelAdmin):
+    list_display = ["title", "published_on"]
+    list_filter = ["published_on", "query"]
+    ordering = ["-published_on"]
+
+
+@admin.register(SearchQuery)
+class SearchQueryAdmin(admin.ModelAdmin):
+    list_display = ["keyword", "last_fetched_on"]
+    readonly_fields = ["keyword", "last_fetched_on"]

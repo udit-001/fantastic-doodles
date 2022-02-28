29567d1676e2e349eb4579f96bd67a4c340bbe91
Udit Mittal <23015406+udit-001@users.noreply.github.com>
2022-02-28 22:54:24 +0530

    Add last_fetched_on field to track task runs per query

--- youtube_search/videos/models.py
+++ youtube_search/videos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class SearchQuery(models.Model):
     keyword = models.CharField(max_length=200)
+    last_fetched_on = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.keyword

17ad338637eda05c6ccf6e450e558e69fac53c28
Udit Mittal <23015406+udit-001@users.noreply.github.com>
2022-02-28 08:37:52 +0530

    Update model definitions and its migration files

--- youtube_search/videos/models.py
+++ youtube_search/videos/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class SearchQuery(models.Model):
+    keyword = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.keyword
+
+class VideoResult(models.Model):
+    query = models.ForeignKey("videos.SearchQuery", on_delete=models.SET_NULL, null=True, related_name="videos")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    link = models.URLField()
+    thumbnail_url = models.URLField()
+    published_on = models.DateTimeField()
+
+    def __str__(self):
+        return self.link
